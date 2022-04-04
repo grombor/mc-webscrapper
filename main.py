d3f5d7a3fe8dab8f6d0426fd467f4adddf7012b4
grombor <gromgaming@yahoo.com>
2022-04-04 20:26:20 +0200

    initial commit for google colab

--- main.py
+++ main.py
@@ -1,4 +1,80 @@
-from modules.mk import print_result
+from bs4 import BeautifulSoup as bs4
+import requests
+from datetime import datetime
 
-if __name__ == "__main__":
-    print_result()
+# variables
+results = []
+links = []
+
+# functions for all sections
+def scrap_mk(touple):
+    
+  # Use requests to retrieve data from a given URL
+  response = requests.get(touple[0])
+
+  # Parse the whole HTML page using BeautifulSoup
+  soup = bs4(response.text, 'html.parser')
+
+  # Parse product name
+  product_name = soup.find('span', {'itemprop': 'name'}).string
+  
+  # Previous product price
+  pprice = touple[1]
+
+  # Current product price
+  cprice = soup.find('span',{'class': 'price'}).string
+
+  # Parse price text
+  cprice = str(cprice).split(',')
+  cprice = cprice[0].replace(u'\xa0', u'')
+
+  if pprice == cprice:
+    message_suffix = ""
+  else:
+    # Calculate diff in prices in precent
+    percent = str((int(cprice) / int(pprice))*100).split(".")
+    message_suffix = f"\tróżni się od poprzednio odnotowanej ceny ({pprice} brutto) o {percent[0]}%\n"
+
+  # Save current date
+  date = datetime.now().strftime("%x")
+  
+  # Save results of scrapping as dict
+  result = {}
+  result["link"] = touple[0]
+  result["odpowiednik"] = touple[2] + f" ( {str(product_name)} )"
+  result["cena"] = cprice
+  result["data"] = date
+  result["msg"] = f"Metalkas: odpowiednik {result['odpowiednik']}\n{message_suffix}"
+  # TODO: add more fields from competitors database
+
+  return results.append(result)
+
+def scrapp_all_links_mk():
+    for link in links:
+        scrap_mk(link)
+
+
+# List of Metalkas links to scrapp.
+# structure: <link> <price> <equivalent model>
+links = [
+  ("https://sklep.metalkas.com.pl/szafa-ubraniowa-2-msu-eco-mtd.html", "500", "Sum 320 W"),
+  ("https://sklep.metalkas.com.pl/tg-5msu-eco.html", "600", "Sus 333 W"),
+]
+
+
+def print_result():
+  scrapp_all_links_mk()
+  for result in results:
+        print(result["msg"])
+
+   
+
+
+
+
+print_result()
+
+
+
+
+
--- modules/utils.py
+++
@@ -1,2 +0,0 @@
-  # TODO: if save_to_file == True : create .csv record using result dict -> create fuction that prepares that record to copy/paste
-  # TODO: create function that calculate how much prices differ in %
