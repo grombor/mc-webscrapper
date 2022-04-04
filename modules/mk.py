edcd1677d6e26fd690cebf42885725416f41f106
grombor <gromgaming@yahoo.com>
2022-04-04 19:18:28 +0200

    added calculating diff in % and some styling

--- modules/mk.py
+++ modules/mk.py
@@ -1,12 +1,14 @@
 from bs4 import BeautifulSoup as bs4
 import requests
+from datetime import datetime
 
 # List of link to scrapp
 links = [
-  "https://sklep.metalkas.com.pl/szafa-ubraniowa-2-msu-eco-mtd.html"
+  ("https://sklep.metalkas.com.pl/szafa-ubraniowa-2-msu-eco-mtd.html", "500", "Sum 320 W"),
+  ("https://sklep.metalkas.com.pl/tg-5msu-eco.html", "600", "Sus 333 W"),
 ]
 
-# List of results
+# Results: list of dict
 results = []
 
 # Message text
@@ -15,10 +17,10 @@ message = "Metalkas: cena odpowiednika Sum 320 W "
 # Metalkas's product URL
 #url = 'https://sklep.metalkas.com.pl/szafa-ubraniowa-2-msu-eco-mtd.html'
 
-def scrap(url):
+def scrap(touple):
 
   # Use requests to retrieve data from a given URL
-  response = requests.get(url)
+  response = requests.get(touple[0])
 
   # Parse the whole HTML page using BeautifulSoup
   soup = bs4(response.text, 'html.parser')
@@ -27,20 +29,35 @@ def scrap(url):
   product_name = soup.find('span', {'itemprop': 'name'}).string
   
   # Previous product price
-  pprice = '605'
+  pprice = touple[1]
 
   # Current product price
   cprice = soup.find('span',{'class': 'price'}).string
 
   # Parse price text
   cprice = str(cprice).split(',')
+  cprice = cprice[0].replace(u'\xa0', u'')
 
-  if pprice == cprice[0]:
-    message2 = " nie różni się od poprzedniej ceny."
+  if pprice == cprice:
+    message_suffix = ""
   else:
-    message2 = f" różni się od poprzedniej ceny ({pprice} brutto)"
+    # Calculate diff in prices in precent
+    percent = str((int(cprice) / int(pprice))*100).split(".")
+    message_suffix = f"\tróżni się od poprzednio odnotowanej ceny ({pprice} brutto) o {percent[0]}%\n"
 
-  return results.append(message + str(product_name) + message2)
+  # Save current date
+  date = datetime.now().strftime("%x")
+  
+  # Save results of scrapping as dict
+  result = {}
+  result["link"] = touple[0]
+  result["odpowiednik"] = touple[2] + f" ( {str(product_name)} )"
+  result["cena"] = cprice
+  result["data"] = date
+  result["msg"] = f"Metalkas: odpowiednik {result['odpowiednik']}\n{message_suffix}"
+  # TODO: add more fields from competitors database
+
+  return results.append(result)
 
 
 def scrapp_all():
@@ -50,4 +67,6 @@ def scrapp_all():
 
 def print_result():
   scrapp_all()
-  print(results)
+  #print(results)
+  for result in results:
+        print(result["msg"])
---
+++ modules/utils.py
@@ -0,0 +1,2 @@
+  # TODO: if save_to_file == True : create .csv record using result dict -> create fuction that prepares that record to copy/paste
+  # TODO: create function that calculate how much prices differ in %
