b503f4d40b4e11ec9d2825075c894840d337401a
grombor <gromgaming@yahoo.com>
2022-04-07 23:05:35 +0200

    added lists of links for Metalkas Umstahl Locobox

--- modules/us.py
+++ modules/us.py
@@ -5,8 +5,12 @@ from datetime import datetime
 # List of link to scrapp
 # Structure <link>, <price>, <equivalent>
 links = [
-  ("https://umstahl.pl/metalowa-szafka-ubraniowa-se-30r2-24h,id102.html", "500", "Sum 320 W"),
- ("https://umstahl.pl/metalowa-szafa-ubraniowa-sle-40r2-24h,id104.html", "1024", "Sus 333 W"),
+ ("https://umstahl.pl/metalowa-szafa-ubraniowa-sle-30r2,id403.html", "1102", "Sul 32 W"),
+ ("https://umstahl.pl/metalowa-szafa-ubraniowa-z-podzialem-wewnetrznym-se-40r2-24h,id550.html", "721", "Sum 420 W"),
+ ("https://umstahl.pl/metalowa-szafka-ubraniowa-se-30r2-24h,id102.html", "459", "Sum 320 W"),
+ ("https://umstahl.pl/metalowa-szafa-kartotekowa-a4-4ra4-24h,id540.html", "983", "Szk 301"),
+ ("https://umstahl.pl/szafa-metalowa-aktowa-m-1ola-500,id199.html", "907", "Sbm 201 M"),
+ ("https://umstahl.pl/szafa-metalowa-aktowa-m-2ola-1000,id53.html", "1037", "Sbm 203 M"),
 ]
 
 # Results: list of dict
