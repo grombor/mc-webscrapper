07b5676ff023886db9eb0100a1c58ab1c795e430
grombor <gromgaming@yahoo.com>
2022-04-07 22:15:44 +0200

    added metalkas list of links

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ from modules.lb import print_result as lb
 
 if __name__ == "__main__":
     mk()
-    us()
-    lb()
+    #us()
+    #lb()
--- modules/mk.py
+++ modules/mk.py
@@ -5,8 +5,14 @@ from datetime import datetime
 # List of link to scrapp
 # Structure <link>, <price>, <equivalent>
 links = [
-  ("https://sklep.metalkas.com.pl/szafa-ubraniowa-2-msu-eco-mtd.html", "500", "Sum 320 W"),
-  ("https://sklep.metalkas.com.pl/tg-5msu-eco.html", "600", "Sus 333 W"),
+  ("https://sklep.metalkas.com.pl/szafy-schowkowe-metalowe-tg-4mss-eco.html", "718", "Sus 322 W"),
+  ("https://sklep.metalkas.com.pl/szafa-ubraniowa-2-msu.html", "807", "Sum 320 W"),
+  ("https://sklep.metalkas.com.pl/szafa-ubraniowa-3-msu-eco.html", "897", "Sum 420 W"),
+  ("https://sklep.metalkas.com.pl/szafa-ubraniowa-tg-2msl-eco-basic.html", "1011", "Sul 32 W"),
+  ("https://sklep.metalkas.com.pl/szafa-biurowa-1-sdb-eco.html", "1252", "Sbm 201 M"),
+  ("https://sklep.metalkas.com.pl/szafa-biurowa-3-sdb-eco.html", "1198", "Sbm 203 M"),
+  ("https://sklep.metalkas.com.pl/szafa-kartotekowa-3-sdk.html", "2424", "Szk 301"),
+  ("https://sklep.metalkas.com.pl/szafa-kartotekowa-szufladowa-tg-2sdk.html", "1888", "Szk 301"),
 ]
 
 # Results: list of dict
