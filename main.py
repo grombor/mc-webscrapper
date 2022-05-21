89289a02bfa60ee41afbbf61e9c4768b82c86aab
grombor <gromgaming@yahoo.com>
2022-05-21 23:05:53 +0200

    added Jan Nowak links and logic

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ from modules.lb import get_results as Locobox
 from modules.bp import get_results as Bakpol
 from modules.mk import get_results as Metalkas
 from modules.us import get_results as Umstahl
+from modules.jn import get_results as JanNowak
 from modules.utils import *
 
 if __name__ == "__main__":
-    Kartmap()
-    Locobox()
-    Bakpol()
-    Metalkas()
-    Umstahl()
+    # Kartmap()
+    # Locobox()
+    # Bakpol()
+    # Metalkas()
+    # Umstahl()
+    JanNowak()
     
     write_to_csv_file()
     
